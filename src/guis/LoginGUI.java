c6ed3a2f96fd05354c4188e529eecb9574020830
Richard Zhang <yonghuz1@uci.edu>
2024-05-25 16:40:11 -0700

    Add username's label and textfield, password's label and textfield, login button and a register label to the login GUI.

--- src/guis/LoginGUI.java
+++ src/guis/LoginGUI.java
@@ -14,12 +14,12 @@ public class LoginGUI extends BaseFrame{
     }
 
     @Override
-    protected void addGuiComponents(){
+    protected void addGuiComponents() {
         // create banking label
         JLabel bankingLabel = new JLabel("Banking");
 
         // set the location and the size of the gui component
-        bankingLabel.setBounds(0,20,super.getWidth(), 40);
+        bankingLabel.setBounds(0, 20, super.getWidth(), 40);
 
         // change the font style
         bankingLabel.setFont(new Font("Dialog", Font.BOLD, 32));
@@ -29,5 +29,58 @@ public class LoginGUI extends BaseFrame{
 
         // add to gui
         add(bankingLabel);
+
+        //user name Label
+        JLabel usernameLabel = new JLabel("Username:");
+
+        // set the user's JLabel location and size
+        usernameLabel.setBounds(20, 120, getWidth() - 30, 24);
+
+        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(usernameLabel);
+
+        //create JTextField for username input
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(20, 160, getWidth() - 50, 40);
+        usernameField.setFont(new Font("Dialog", Font.PLAIN, 28));
+        add(usernameField);
+
+        //create JLabel for password
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(20, 280, getWidth(), 24);
+        passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(passwordLabel);
+
+        //create JTextField for password input
+        JTextField passwordField = new JTextField();
+        passwordField.setBounds(20, 320, getWidth() - 50, 40);
+        passwordField.setFont(new Font("Dialog", Font.PLAIN, 28));
+        add(passwordField);
+
+        // create login button
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(20, 460,getWidth() - 50, 40);
+        loginButton.setFont(new Font("Dialog", Font.BOLD, 20));
+        add(loginButton);
+
+        // create register label
+        JLabel registerLabel = new JLabel("<html><a href=\"#\">Don't have an account? Register Here</a></html");
+        registerLabel.setBounds(0, 510, getWidth() - 10, 30);
+        registerLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        registerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(loginButton);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
