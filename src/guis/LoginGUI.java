f4d8bba46695d4ec1df12b681bbfccd3774379a1
Richard Zhang <yonghuz1@uci.edu>
2024-05-25 16:18:29 -0700

    Add a title to Login page.

--- src/guis/LoginGUI.java
+++ src/guis/LoginGUI.java
@@ -1,5 +1,9 @@
 package guis;
 
+
+import javax.swing.*;
+import java.awt.*;
+
 /*
     This gui will allow user to login or launch the register GUI
     This extends from the BaseFrame
@@ -11,6 +15,19 @@ public class LoginGUI extends BaseFrame{
 
     @Override
     protected void addGuiComponents(){
+        // create banking label
+        JLabel bankingLabel = new JLabel("Banking");
 
+        // set the location and the size of the gui component
+        bankingLabel.setBounds(0,20,super.getWidth(), 40);
+
+        // change the font style
+        bankingLabel.setFont(new Font("Dialog", Font.BOLD, 32));
+
+        // center text in JLabel
+        bankingLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        // add to gui
+        add(bankingLabel);
     }
 }
